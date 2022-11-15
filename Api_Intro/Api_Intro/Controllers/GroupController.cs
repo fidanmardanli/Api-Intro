48cb483ab698f57ca6e504eb2885605c9a50a13a
fidanmardanli <fidannm@code.edu.az>
2022-11-16 01:10:12 +0400

    task

---
+++ Api_Intro/Api_Intro/Controllers/GroupController.cs
@@ -0,0 +1,46 @@
+﻿using Api_Intro.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api_Intro.Controllers
+{
+    [Route("api/[controller]/")]
+    [ApiController]
+    public class GroupController : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("GetAll")]
+        public IActionResult GetAll()
+        {
+            List<Group> groups = new List<Group>();
+
+            Group group1 = new()
+            {
+                Id = 1,
+                Name = "A",
+                Capasity = 16
+            };
+
+            Group group2 = new()
+            {
+                Id = 2,
+                Name = "B",
+                Capasity = 14
+            };
+
+            Group group3 = new()
+            {
+                Id = 3,
+                Name = "C",
+                Capasity = 13
+            };
+
+            groups.Add(group1);
+            groups.Add(group2);
+            groups.Add(group3);
+
+            return Ok(groups);
+        }
+    }
+}
---
+++ Api_Intro/Api_Intro/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api_Intro.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class ProductController : ControllerBase
+    {
+        //public IActionResult Index()
+        //{
+        //    return Ok();
+        //}
+        [HttpGet]
+        //public string GetNameById([FromForm] int id)
+        //{
+        //    return "Fidash" + id;
+        //}
+
+        public IActionResult GetById( int id)
+        {
+            string name = "Fidash" + id;
+            return Ok(name);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<string> names = new List<string>() { "Fidash", "Lalesh"};
+            return Ok(names);
+        }
+    }
+}
